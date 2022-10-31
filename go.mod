c7331129e33f2fc06b90fba5b29d6185a92dd81f
bioculty <bioculty@gmail.com>
2022-10-31 19:10:31 -0300

    site: initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module bioculty/+10
+
+go 1.19
+
+require (
+	github.com/gohugoio/hugo-mod-bootstrap-scss/v5 v5.20200.20100 // indirect
+	github.com/gohugoio/hugo-mod-jslibs-dist/popperjs/v2 v2.21100.20000 // indirect
+	github.com/twbs/bootstrap v5.2.2+incompatible // indirect
+)
---
+++ themes/bioculty/assets/js/index.js
@@ -0,0 +1,29 @@
+// Import the Bootstrap components we want to use.
+// See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
+import Toast from "js/bootstrap/src/toast";
+import Popover from "js/bootstrap/src/popover";
+import feather from "feather-icons"
+
+const bootstrapFn = () => {
+  let toastElList = [].slice.call(document.querySelectorAll(".toast"));
+  let toastList = toastElList.map(function (toastEl) {
+    return new Toast(toastEl);
+  });
+  toastList.forEach(function (toast) {
+    toast.show();
+  });
+
+  let popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]')
+  );
+  popoverTriggerList.map(function (popoverTriggerEl) {
+    return new Popover(popoverTriggerEl);
+  });
+}
+
+function main() {
+  bootstrapFn();
+  feather.replace();
+}
+
+main();
